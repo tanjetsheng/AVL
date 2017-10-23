4af28ae49e995972921de0a142df6360c3a854ad
tanjetsheng <tanjetsheng@gmail.com>
2017-10-23 18:33:21 +0800

    added leftright

--- src/rotate.c
+++ src/rotate.c
@@ -8,12 +8,20 @@ Node *rotateRight(Node *node){
   root = node->left;
   node->left = node->left->right;
   root ->right = node;
-
+  return root;
 }
 
 Node *rotateLeft(Node *node){
   Node *root;
-  root = node->left->right;
-  root ->right = node;
+  root = node->right;
+  node->right = root->left;
+  root->left = node;
+  return root;
+}
 
+Node *rotateLeftright(Node *node){
+  Node *root;
+  node->left = rotateLeft(node->left);
+  root = rotateRight(node);
+  return root;
 }
--- test/test_rotate.c
+++ test/test_rotate.c
@@ -38,28 +38,20 @@ void tearDown(void)
 //right
 /**
  *        30            10
- *       /             /  \
- *     10       ---> 5    30
- *    /  \               /
- *  5     20           20
+ *       /   \          /  \
+ *     10     40  ---> 5    30
+ *    /  \                /   \
+ *  5     20            20    40
  *
  */
- //left
- /**
-  *        30            20
-  *       /             /  \
-  *     10       ---> 10    30
-  *    /  \          /
-  *  5     20      5
-  *
-  */
+
 void test_rotateRight(void)
 {
     Node *root;
     initNode(&node5,NULL,NULL,0);
     initNode(&node20,NULL,NULL,0);
     initNode(&node10,&node5,&node20,0);
-    initNode(&node30,&node10,NULL,-2);
+    initNode(&node30,&node10,&node40,-2);
 
     root = rotateRight(&node30);
     TEST_ASSERT_EQUAL_PTR(&node10,root);
@@ -68,8 +60,78 @@ void test_rotateRight(void)
     TEST_ASSERT_EQUAL_PTR(NULL,node5.left);
     TEST_ASSERT_EQUAL_PTR(NULL,node5.right);
     TEST_ASSERT_EQUAL_PTR(&node20,node30.left);
-    TEST_ASSERT_EQUAL_PTR(NULL,node30.right);
+    TEST_ASSERT_EQUAL_PTR(&node40,node30.right);
     TEST_ASSERT_EQUAL_PTR(&node5,node10.left);
     TEST_ASSERT_EQUAL_PTR(&node30,node10.right);
 
+}
+
+//left
+/**
+---------rotateleft-----------------
+*        10            30
+*         \           /   \
+*          30  ->    10    40
+*         / \         \
+*        25  40       25
+*
+**/
+
+
+void test_rotateLeft(void)
+{
+    Node *root;
+    initNode(&node25,NULL,NULL,0);
+    initNode(&node40,NULL,NULL,0);
+    initNode(&node30,&node25,&node40,0);
+    initNode(&node10,NULL,&node30,-2);
+
+    root = rotateLeft(&node10);
+    TEST_ASSERT_EQUAL_PTR(&node30,root);
+    TEST_ASSERT_EQUAL_PTR(NULL,node25.left);
+    TEST_ASSERT_EQUAL_PTR(NULL,node25.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node10.left);
+    TEST_ASSERT_EQUAL_PTR(&node25,node10.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node40.left);
+    TEST_ASSERT_EQUAL_PTR(NULL,node40.right);
+    TEST_ASSERT_EQUAL_PTR(&node10,node30.left);
+    TEST_ASSERT_EQUAL_PTR(&node40,node30.right);
+}
+
+/**
+---------rotateleftright-----------------
+*        45                   45                   30
+*       /  \      RL"10"    /   \      RR"45"     /   \
+*      10   50  ------->  30    50  ----------> 10     45
+*        \               /  \                     \    / \
+*        30             10   40                   25  40 50
+*       /  \              \
+*      25   40             25
+*
+**/
+
+void test_rotateLeftRight(void)
+{
+    Node *root;
+    initNode(&node25,NULL,NULL,0);
+    initNode(&node40,NULL,NULL,0);
+    initNode(&node30,&node25,&node40,0);
+    initNode(&node10,NULL,&node30,-2);
+    initNode(&node45,&node10,&node50,-2);
+    initNode(&node50,NULL,NULL,0);
+
+    root = rotateLeftright(&node45);
+    TEST_ASSERT_EQUAL_PTR(&node30,root);
+    TEST_ASSERT_EQUAL_PTR(NULL,node25.left);
+    TEST_ASSERT_EQUAL_PTR(NULL,node25.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node10.left);
+    TEST_ASSERT_EQUAL_PTR(&node25,node10.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node40.left);
+    TEST_ASSERT_EQUAL_PTR(NULL,node40.right);
+    TEST_ASSERT_EQUAL_PTR(&node10,node30.left);
+    TEST_ASSERT_EQUAL_PTR(&node45,node30.right);
+    TEST_ASSERT_EQUAL_PTR(&node40,node45.left);
+    TEST_ASSERT_EQUAL_PTR(&node50,node45.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node50.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node50.right);
 }
