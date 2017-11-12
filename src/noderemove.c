9747b7efd8357e9ee95afc6240d5c480c86fc8b3
tanjetsheng <tanjetsheng@gmail.com>
2017-11-13 00:15:30 +0800

    added remove without bf but have algorithm problem

--- src/noderemove.c
+++ src/noderemove.c
@@ -13,12 +13,36 @@ Node *getValue(Node *nodePtr){
   }
 }
 Node *removeNode(Node **nodePtr,Node *nodeToRemove){
-  if(*nodePtr == nodeToRemove){
-      *nodePtr = NULL;
+  if((*nodePtr)->data > (nodeToRemove)->data){
+    *nodePtr = removeNode((&(*nodePtr)->left),nodeToRemove);
   }
-  else if((*nodePtr)->left->data == nodeToRemove->data){
-    (*nodePtr)->left = NULL;
-    (*nodePtr)->bf += 1;
+  else if((*nodePtr)->data < (nodeToRemove)->data){
+    *nodePtr = removeNode((&(*nodePtr)->right),nodeToRemove);
+  }
+  else if((*nodePtr)->data == (nodeToRemove)->data){
+    if(((*nodePtr)->left == NULL) && ((*nodePtr)->right == NULL)){
+      *nodePtr = NULL;
+    }
+    else if(((*nodePtr)->left != NULL) && ((*nodePtr)->right != NULL)){
+    //  Node *root = (*nodePtr)->right;
+      Node *temp = getValue((*nodePtr)->right);
+     removeNode(&(*nodePtr)->right,temp);
+      temp->left = (*nodePtr)->left;
+      temp->right = (*nodePtr)->right;
+      *nodePtr = temp;
+    //  return *nodePtr;
+
+    }
+      else if(((*nodePtr)->left != NULL) && ((*nodePtr)->right == NULL)){
+      Node *root = (*nodePtr)->left;
+      Node *temp = getValue(root);
+      (*nodePtr)->left = removeNode(&(*nodePtr)->left,temp);
+      temp->left = (*nodePtr)->left;
+      temp->right = (*nodePtr)->right;
+      *nodePtr = temp;
+      //return *nodePtr;
+
+    }
   }
   return *nodePtr;
 }
--- test/test_noderemove.c
+++ test/test_noderemove.c
@@ -33,11 +33,109 @@ void tearDown(void)
 {
 }
 
+
+
+
+
+
 void test_noderemove_remove(void)
 {
     Node *root = &node10;
-    initNode(&node10,NULL,NULL,-1);
-//    initNode(&node5,NULL,NULL,0);
+    initNode(&node10,NULL,NULL,0);
     removeNode(&root,&node10);
     TEST_ASSERT_EQUAL_PTR(NULL,root);
+}
+/*
+          20                      20
+        /   \                   /   \
+      10    30   -->          5     30
+            /
+          15
+*/
+void test_noderemovsdsadsadsad(void)
+{
+    Node *root = &node20;
+    initNode(&node20,&node10,&node30,-1);
+    initNode(&node10,NULL,NULL,0);
+    initNode(&node15,NULL,NULL,0);
+    initNode(&node30,&node15,NULL,-1);
+    removeNode(&root,&node20);
+    TEST_ASSERT_EQUAL_PTR(&node10,node15.left);
+    TEST_ASSERT_EQUAL_PTR(&node30,node15.right);
+}
+
+/*
+          20                      20
+        /   \                   /   \
+      10    30   -->          5     30
+    /
+  5
+*/
+void test_noderemove_remove_with_right_value_is_NULL(void)
+{
+    Node *root = &node20;
+    initNode(&node20,&node10,&node30,-1);
+    initNode(&node10,&node5,NULL,-1);
+    initNode(&node5,NULL,NULL,0);
+    initNode(&node30,NULL,NULL,0);
+    removeNode(&root,&node10);
+    TEST_ASSERT_EQUAL_PTR(&node5,node20.left);
+    TEST_ASSERT_EQUAL_PTR(NULL,node5.left);
+}
+
+/*
+          20                      20
+        /   \                   /   \
+      10    30   -->          15     30
+    /   \                     /
+  5      15                 5
+*/
+void test_noderemove_remove_replace_with_right_value(void)
+{
+    Node *root = &node20;
+    initNode(&node20,&node10,&node30,-1);
+    initNode(&node10,&node5,&node15,0);
+    initNode(&node5,NULL,NULL,0);
+    initNode(&node30,NULL,NULL,0);
+    initNode(&node15,NULL,NULL,0);
+    removeNode(&root,&node10);
+    TEST_ASSERT_EQUAL_PTR(&node15,node20.left);
+    TEST_ASSERT_EQUAL_PTR(&node5,node15.left);
+    TEST_ASSERT_EQUAL_PTR(NULL,node15.right);
+
+}
+
+/*
+          25                      25
+        /   \                   /   \
+      10    30   -->          15     30
+    /   \     \              /  \     \
+  5      20    35          5    20     35
+        /
+      15
+*/
+void test_noderemove_remove_replace_with_right_value2(void)
+{
+    Node *root = &node25;
+    initNode(&node25,&node10,&node30,-1);
+    initNode(&node10,&node5,&node20,1);
+    initNode(&node5,NULL,NULL,0);
+    initNode(&node30,NULL,&node35,1);
+    initNode(&node20,&node15,NULL,-1);
+    initNode(&node15,NULL,NULL,0);
+    initNode(&node35,NULL,NULL,0);
+    removeNode(&root,&node10);
+    TEST_ASSERT_EQUAL_PTR(&node15,node25.left);
+    TEST_ASSERT_EQUAL_PTR(&node30,node25.right);
+    TEST_ASSERT_EQUAL_PTR(&node5,node15.left);
+    TEST_ASSERT_EQUAL_PTR(&node20,node15.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node30.left);
+    TEST_ASSERT_EQUAL_PTR(&node35,node30.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node5.left);
+    TEST_ASSERT_EQUAL_PTR(NULL,node5.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node20.left);
+    TEST_ASSERT_EQUAL_PTR(NULL,node20.right);
+    TEST_ASSERT_EQUAL_PTR(NULL,node35.left);
+    TEST_ASSERT_EQUAL_PTR(NULL,node35.right);
+
 }
